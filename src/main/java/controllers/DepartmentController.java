5d5a56cbbdb46605acff819f06891afa7a385016
Jonathan Cruickshank <ikenna1982@hotmail.com>
2018-03-20 17:45:47 +0000

    adds post and get to make new department, and create.vtl

--- src/main/java/controllers/DepartmentController.java
+++ src/main/java/controllers/DepartmentController.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import com.sun.tools.internal.xjc.model.Model;
 import db.DBHelper;
 import models.Department;
 import org.apache.velocity.app.Velocity;
@@ -7,6 +8,7 @@ import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 
 import static spark.Spark.get;
+import static spark.Spark.post;
 
 import java.util.HashMap;
 import java.util.List;
@@ -26,6 +28,20 @@ public class DepartmentController {
                 model.put("departments", departments);
                 model.put("template", "templates/departments/index.vtl");
                 return new ModelAndView(model, "templates/layout.vtl");
+            }, velocityTemplateEngine);
+
+            get("/departments/new", (req, res) -> {
+                HashMap<String, Object> model = new HashMap<>();
+                model.put("template", "templates/departments/create.vtl");
+                return new ModelAndView(model, "templates/layout.vtl");
+            }, velocityTemplateEngine);
+
+            post("/departments", (req, res) -> {
+                String title = req.queryParams("title");
+                Department department = new Department(title);
+                DBHelper.save(department);
+                res.redirect("/departments");
+                return null;
             }, velocityTemplateEngine);
         }
 
